6f4d4b731fe30947f091ae1b884081eefb3762a4
Matthew Brown <matthewbrwn2005@gmail.com>
2026-05-04 17:44:27 -0400

    store new addictions in array

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const addictionInput = document.getElementById("addictionInput")
 const addAddictionBtn= document.getElementById("addAddictionBtn")
+const addictionsContainer = document.getElementById("addictionsContainer")
 
 
 let addictions = []
-let selectedAddicitonId = null 
+
+addAddictionBtn.addEventListener("click", handleAddAddiction)
 
 function handleAddAddiction(){
+    let addictionName = addictionInput.value.trim()
 
-}
+    if(!addictionName){
+    return
+    }
+
+    const newAddiction = {
+        name: addictionName, 
+        id: Date.now(),
+        entries: []
+    }
+
+    addictions.push(newAddiction)
+
+    addictionInput.value = ""
+
+    console.log(addictions)
+
+}   
